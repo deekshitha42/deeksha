c1584d601fd52229b3aa0d8ab4834add6312c8ea
deekshitha42 <deekshugowda228@gmail.com>
2024-08-23 14:18:10 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 14:16:18 2024
+
+@author: HP
+"""
+
+numberl = 100
+print(”The type of number1 ”,type(number 1))
+integer_1 = 100
+integer_2 = 50
+print (integer_1*integer_2)
+print(integer_1+integer_2) 
+print(integer_1-integer_2) 
+print(integer_1/integer_2)
+number2 = 100.0
+print(”The type of number2 ”,type(number 2)) 
+float_1 = 12.539 
+float_2 = 6.78
+print (float_1*float_2)
+print(float_1+float_2) 
